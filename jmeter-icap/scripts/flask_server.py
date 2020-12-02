f0a462fce005f19ed98dff295b52459a8d05082b
Ali J <59252902+Ali-J2@users.noreply.github.com>
2020-12-02 22:12:34 +0300

    Ali: Added Flask service file, exec.sh, and instructions on how to deploy flask server as service along with bug fixes (#173)
    * Slight change to UI, changed flask_server to use new test script
    
    - flask_server: now uses run_local_test instead of create_stack_dash
    - UI: Placeholder of "prefix" form field now reads "ex: example-prefix" instead of "demo"
    
    * Slight change to UI, changed flask_server to use new test script
    
    - flask_server: now uses run_local_test instead of create_stack_dash
    - UI: Placeholder of "prefix" form field now reads "ex: example-prefix" instead of "demo"
    
    * Changed flask_server, added flask service and exec.sh for running server as a service
    
    - flask.service: Used to tell ubuntu OS to start flask server as a service
    - exec.sh: Used by flask.service to run a script execution command (runs flask_server.py)
    - flask_server: Uses waitress python package to start production server rather than built in dev server that flask uses
    
    * Update create_stack_dash.py
    
    removed unused method, unnecessary print statements
    
    * updated UI, scripts
    
    - run_local_test: Now uses Grafana URL provided in config.env, NOT localhost:3000
    - flask.service: updated file location of scripts
    - UI: Now opens grafana link in new tab, title of page changed to "ICAP Performance Test"
    
    * Create requirements.txt
    
    added requirements file which will make it easier to install required packages for projects
    
    * Updates to instructions, create_stack_dash
    
    create_stack_dash: removed unnecessary print statements formerly used for debugging
    -instructions: Added section explaining how to use requirements.txt, flask.service, and exec.sh. These are used for setting up the flask server as a service
    
    * Update angular-ui-component-install-and-deploy.md
    
    * Update angular-ui-component-install-and-deploy.md

--- jmeter-icap/scripts/flask_server.py
+++ jmeter-icap/scripts/flask_server.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 import json
-from create_stack_dash import run_using_json
 import run_local_test
+from waitress import serve
 
 UPLOAD_FOLDER = './'
 ALLOWED_EXTENSIONS = {'csv'}
@@ -14,21 +14,9 @@ def parse_request():
     data = json.loads(request.form.get('form'))
     print(data)
 
-    # if 'file' not in request.files:
-    #     return redirect(request.url)
-    # file = request.files['file']
-    # if user does not select file, browser also
-    # submit an empty part without filename
-    # if file.filename == '':
-    #     return redirect(request.url)
-    # if file:
-    #     filename = secure_filename(file.filename)
-    #     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-
     returned_url = run_local_test.main(data)
     if returned_url:
         return make_response(jsonify(returned_url), 201)
 
 CORS(app)
-app.run()
+serve(app, host='0.0.0.0', port=5000)
