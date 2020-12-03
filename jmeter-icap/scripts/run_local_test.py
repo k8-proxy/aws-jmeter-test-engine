aacf78f15a9f26b847ee11cca95f85c96d3e638d
Ali-J2 <a.jiraki@gmail.com>
2020-12-03 18:39:50 +0300

    Update run_local_test.py

--- jmeter-icap/scripts/run_local_test.py
+++ jmeter-icap/scripts/run_local_test.py
@@ -95,11 +95,8 @@ def determine_tls_and_port_params(input_load_type, input_enable_tls, input_tls_i
     if input_load_type == "Direct":
 
         # enable/disable tls based on user input
-        Config.enable_tls = input_enable_tls
-        if input_enable_tls:
-            Config.enable_tls = "true"
-        else:
-            Config.enable_tls= "false"
+        Config.enable_tls = str(input_enable_tls).lower()
+        print(Config.enable_tls)
 
         # if user entered a port, use that. Otherwise port will be set depending on tls_enabled below.
         if input_port:
