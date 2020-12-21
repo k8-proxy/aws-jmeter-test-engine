8868f218816a3e4622054c98d2b8e407bea40c91
Shashank Sinha <sinha.shashank.1989@gmail.com>
2020-12-21 18:59:46 +0400

    remove password

--- jmeter-icap/deployment/packer/ova-check.sh
+++ jmeter-icap/deployment/packer/ova-check.sh
@@ -22,4 +22,4 @@ echo "Downloading OVA..."
 aws s3 cp s3://$BUCKET/vms/$OBJECT $OBJECT --profile $PROFILE
 echo "Download Complete !!!"
 echo "Uploading OVA to ESXI..."
-ovftool --name=Packer-$OBJECT --disableVerification  --noSSLVerify --datastore=datastore1 --network="VMs" $OBJECT  vi://root:s9q_5d.%AVQg@46.165.225.145
+ovftool --name=Packer-$OBJECT --disableVerification  --noSSLVerify --datastore=datastore1 --network="VMs" $OBJECT  vi://root:<password>@46.165.225.145
