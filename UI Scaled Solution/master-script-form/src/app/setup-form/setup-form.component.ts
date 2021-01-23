bd95b9d0a57f6e2bb52c7db49c6681974d8957cb
Ali J <a.jiraki@gmail.com>
2021-01-23 13:54:06 +0300

    Generated new components for feature

---
+++ UI Scaled Solution/master-script-form/src/app/setup-form/setup-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'setup-form',
+  templateUrl: './setup-form.component.html',
+  styleUrls: ['./setup-form.component.css']
+})
+export class SetupFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
