d3bffe68d3e714a78005aa096ea309ce32eecdd8
Shashank Sinha <sinha.shashank.1989@gmail.com>
2021-02-01 17:33:29 +0400

    update ui script

---
+++ jmeter-icap/deployment/packer/provisioning_scripts/ui_scaled.sh
@@ -0,0 +1,44 @@
+sudo apt install nodejs -y
+sudo apt install npm -y
+sudo npm install -g @angular/cli
+sudo npm install -g http-server
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y python3-pip
+sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
+sudo apt update
+sudo apt install -y apache2
+sudo systemctl status apache2
+sudo systemctl enable apache2
+cd /opt/git/aws-jmeter-test-engine/jmeter-icap/scripts/
+sudo pip3 install -r requirements.txt
+cd /opt/git/aws-jmeter-test-engine/UI/master-script-form/
+sudo npm install
+cd /opt/git/aws-jmeter-test-engine/UI/master-script-form/
+sudo ng build --prod
+sudo cp -a /opt/git/aws-jmeter-test-engine/UI/master-script-form/dist/master-script-form/. /var/www/html/
+cd /opt/git/aws-jmeter-test-engine/jmeter-icap/scripts/
+sudo chmod +x exec.sh
+sudo bash -c 'cat << EOF >> /etc/systemd/system/flask_scaled.service
+[Unit]
+Description=WSGI App for ICAP Testing UI Front End
+After=network.target
+
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/opt/git/aws-jmeter-test-engine/jmeter-icap/scripts
+ExecStart=/opt/git/aws-jmeter-test-engine/jmeter-icap/scripts/exec_scaled.sh
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+sudo systemctl daemon-reload
+sudo systemctl enable flask_scaled
+sudo systemctl start flask_scaled
+sudo systemctl status flask_scaled
+sleep 10
+sudo netstat  -ntlp
+cd /opt/git/aws-jmeter-test-engine/jmeter-icap/scripts
+sudo ./changeIP.sh
