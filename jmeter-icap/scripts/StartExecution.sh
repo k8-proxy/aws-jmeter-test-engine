8d97d0b1195d99d12083689a50246b016faad42f
jpoolGW <jpool@glasswallsolutions.com>
2021-02-03 20:21:21 +0000

    Replace hard coded secret name with paramerterised secret name

--- jmeter-icap/scripts/StartExecution.sh
+++ jmeter-icap/scripts/StartExecution.sh
@@ -12,8 +12,8 @@ sudo aws s3 cp s3://$SCRIPT_BUCKET/script/$DATA_FILE /home/ec2-user/apache-jmete
 sudo aws s3 cp s3://$SCRIPT_BUCKET/script/lib/ /home/ec2-user/apache-jmeter-5.3/lib/ --recursive
 
 # Get AWS Credentials
-ACCESS_KEY=$(sudo aws secretsmanager get-secret-value --secret-id GlasswallDataRepositoryTestUser --region $REGION | jq -c '.SecretString | fromjson' | jq -r '.AWS_Access_Key')
-SECRET_KEY=$(sudo aws secretsmanager get-secret-value --secret-id GlasswallDataRepositoryTestUser --region $REGION | jq -c '.SecretString | fromjson' | jq -r '."AWS_Secret_Key "')
+ACCESS_KEY=$(sudo aws secretsmanager get-secret-value --secret-id $SECRET_ID --region $REGION | jq -c '.SecretString | fromjson' | jq -r '.AWS_Access_Key')
+SECRET_KEY=$(sudo aws secretsmanager get-secret-value --secret-id $SECRET_ID --region $REGION | jq -c '.SecretString | fromjson' | jq -r '."AWS_Secret_Key "')
 
 # Start Test Execution
 sudo JVM_ARGS="-Xms9216m -Xmx9216m" sh jmeter.sh -n -t $SCRIPT -Jp_vuserCount=4000 -Jp_filetype=$DATA_FILE -Jp_rampup=300 -Jp_duration=900 -Jp_aws_access_key=$ACCESS_KEY -Jp_aws_secret_key=$SECRET_KEY -Jp_bucket=$TEST_DATA_BUCKET -Jp_influxHost=10.112.0.112 -Jp_aws_region=$REGION -Jp_url=gw-icap01.westeurope.azurecontainer.io -Jp_prefix=glasswall -Jp_port=1344 -Jp_use_tls=false -Jp_tls=no-verify -l icaptest-s33.log
