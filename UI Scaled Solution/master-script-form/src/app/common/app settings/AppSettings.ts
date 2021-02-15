d6e0753704396fec34c507361f672f99168ae1a9
Ali-J2 <a.jiraki@gmail.com>
2021-02-15 18:11:35 +0300

    Added functionality to auto-generate test name for test table and dashboard link for REST API

--- UI Scaled Solution/master-script-form/src/app/common/app settings/AppSettings.ts
+++ UI Scaled Solution/master-script-form/src/app/common/app settings/AppSettings.ts
@@ -4,8 +4,8 @@ export class AppSettings {
     public static loadTypeNames: string[] = ['Direct', 'Proxy SharePoint', 'Direct Sharepoint', 'REST API'];
     public static endPointFieldTitles: string[] = ["ICAP Server Endpoint URL*", "SharePoint Endpoint URL*", "SharePoint Endpoint URL*", "REST API Endpoint*", "Proxy IP Address*"];
     public static endPointFieldPlaceholders: string[] = ["Ex: icap-client.region.app.provider.com", "Ex: saas1.sharepoint.com", "Ex: saas1.sharepoint.com", "Rest API Endpoint"]
-    public static testNames: string[] = ["ICAP Live Performance Dashboard", "SharePoint Proxy Live Performance Dashboard", "SharePoint Direct Live Performance Dashboard", "Proxy Site Live Performance Dashboard"];
-    public static dashboardNames: string[] = ["-icap-live-performance-dashboard", "-demo-dashboard-sharepoint", "-sharepoint-direct-live-performance-dashboard", "-proxysite-live-performance-dashboard"];
+    public static testNames: string[] = ["ICAP Live Performance Dashboard", "SharePoint Proxy Live Performance Dashboard", "SharePoint Direct Live Performance Dashboard", "REST API Live Performance Dashboard" ,"Proxy Site Live Performance Dashboard"];
+    public static dashboardNames: string[] = ["-icap-live-performance-dashboard", "-demo-dashboard-sharepoint", "-sharepoint-direct-live-performance-dashboard", "-rest-api-live-performance-dashboard", "-proxysite-live-performance-dashboard"];
     public static cookiesExist: boolean;
     public static testPrefixSet = new Set<string>();
     public static addingPrefix: boolean = false;
